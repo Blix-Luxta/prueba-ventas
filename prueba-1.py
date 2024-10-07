3361ba1899e30929886a93ed2da3189b42ebff37
NAHUEL CAERO <nc7519@mera.latam>
2024-10-07 13:49:45 -0300

    Filtrado ok en archivo copia

--- prueba-1.py
+++ prueba-1.py
@@ -3,11 +3,19 @@ import openpyxl
 
 # Leer un archivo Excel
 df = pd.read_excel('Reporte Operadores s2s - Mera.xlsx', header=6, usecols="B:I")
-
+print(df.shape)
 df.columns = ['PCRC', 'OPERADOR', 'Cod. Agente', 'Ll. ACD', 'LOGUEO', 'Q. Ventas', 'vma', 'Supervisor']
 df['Ll. ACD'] = pd.to_numeric(df['Ll. ACD'], errors='coerce')
-df_filtrado = df[df['Ll. ACD'] > 0]
 
-df_filtrado = df_filtrado.dropna()
 
-print(df_filtrado)
+if 'Ll. ACD' in df:
+    df_filtrado = df[df['Ll. ACD'] != 0]
+    print(df_filtrado.shape)
+else:
+    print("Error", "La columna 'Ll. ACD' no se encuentra en los datos.")
+
+df_filtrado = df_filtrado[df_filtrado["OPERADOR"] != "#N/D"]
+
+print(df_filtrado.shape)
+
+df_filtrado.to_excel('Reporte Operadores s2s - Mera.xlsx', index=False, engine='openpyxl')
